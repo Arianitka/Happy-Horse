23bff79332614b5c08d67a1b3e45a18655a8609a
Arianitka <134192362+Arianitka@users.noreply.github.com>
2024-03-24 23:10:40 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AddThemeComponent } from './theme/add-theme/add-theme.component';
+import { CurrentThemeComponent } from './theme/current-theme/current-theme.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { CoreModule } from './app/core/core.module';
+import { HttpClientModule } from '@angular/common/http';
+import { UserModule } from './user/user.module';
+import { SharedComponent } from './shared/shared.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AddThemeComponent,
+    CurrentThemeComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    SharedComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    // SharedModule,
+    HttpClientModule,
+    UserModule,
   ],
   providers: [
     provideClientHydration()
