b5a4270b65d879a6c0e645b9ca918fe4906a129e
Arianitka <134192362+Arianitka@users.noreply.github.com>
2024-03-27 20:08:43 +0200

    update

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { UserForAuth } from '../types/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  user: UserForAuth | undefined;
+  USER_KEY = '[user]';
 
-  constructor() { }
+  get isLogged() : boolean {
+    return !!this.user;
+  }
+  constructor() {
+    const lsUser = localStorage.getItem(this.USER_KEY)
+    this.user = JSON.parse(lsUser)
+   }
+
+  login() {
+    this.user = {
+      firstName: 'Ariana',
+      email: 'arianadzh@abv.bg',
+      password: '123456',
+      phoneNumber: '123-456-789-123',
+    };
+
+    localStorage.setItem(this.USER_KEY, JSON.stringify(this.user))
+  }
+  
+  logout() {
+    this.user = undefined;
+    localStorage.removeItem(this.USER_KEY);
+  }
 }
