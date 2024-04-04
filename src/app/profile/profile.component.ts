4489e906953820e943c8e6659156b3d37f25a8d5
Arianitka <134192362+Arianitka@users.noreply.github.com>
2024-04-04 19:40:04 +0300

    update

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,6 +1,6 @@
-
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router'; 
 
 @Component({
   selector: 'app-profile',
@@ -10,10 +10,14 @@ import { AuthService } from '../services/auth.service';
 export class ProfileComponent implements OnInit {
   user: any;
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {
-  
-    this.user = this.authService.getCurrentUser(); 
+    this.user = this.authService.getCurrentUser();
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']); 
   }
 }
